0cff7a2ecc47a75022542fb75900cad148648aa5
Marosdee Uma <danya0365@gmail.com>
2026-01-10 13:40:33 +0700

    chore: set timeout

--- src/presentation/presenters/queueStatus/QueueStatusPresenter.ts
+++ src/presentation/presenters/queueStatus/QueueStatusPresenter.ts
@@ -158,7 +158,7 @@ export class QueueStatusPresenter {
   /**
    * Helper to wrap promise with timeout
    */
-  private async withTimeout<T>(promise: Promise<T>, ms: number = 15000): Promise<T> {
+  private async withTimeout<T>(promise: Promise<T>, ms: number = 5000): Promise<T> {
     return Promise.race([
       promise,
       new Promise<T>((_, reject) =>
